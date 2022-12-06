97972f29733b48d044ee1252e5dd55f05c5e49bd
latifaAmini <119930944+latifaAmini@users.noreply.github.com>
2022-12-06 12:27:39 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+
+const menu = document.querySelector("#mobile-menu");
+const menulinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function(){
+    menu.classList.toggle("active");
+    menulinks.classList.toggle("active");
+});
+
+function buy (){
+    document.getElementById("purchase").innerHTML
+}
+
